b81ccfcf0a55c2bff631b6214fc76620645a095f
Nikita Nasevich <nasevich.nik@mail.ru>
2022-12-29 12:38:52 +0300

    first commit

---
+++ src/main/java/ru/springcourse/models/Person.java
@@ -0,0 +1,61 @@
+package ru.springcourse.models;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+public class Person {
+    private int id;
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 30, message = "nor correct")
+    private String name;
+    @Min(value = 0, message = "age should be a positive value")
+    private int age;
+    @NotEmpty(message = "should not be empty")
+    @Email(message = "email should be valid")
+    private String email;
+
+    public Person() {
+
+    }
+
+    public Person(int id, String name, int age, String email) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
